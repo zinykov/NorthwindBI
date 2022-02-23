34e5510a99d05e155f54d08de00f23685a12ba94
Денис Зинуков <zinykov@hotmail.com>
2022-02-23 18:31:37 +0300

    Added Date Dimention
    Связанные рабочие элементы: #81

---
+++ NorthwindDW/Dim/Tables/Date.sql
@@ -0,0 +1,17 @@
+﻿/*
+    Source for Date dimension columns is https://www.daxpatterns.com/standard-time-related-calculations/
+*/
+CREATE TABLE [Dimension].[Date]
+(
+	[DateID]            INT             NOT NULL, -- YYYYMMDD
+    [AlterDateID]       DATE            NOT NULL, 
+    [Year]              DATE            NOT NULL, -- End/Start Of Year date
+    [YearQuarter]       NVARCHAR(10)    NOT NULL, -- QQ-YYYY
+    [YearQuarterDate]   DATE            NOT NULL, --End/Start of Quarter date
+    [Quarter]           NVARCHAR(5)     NOT NULL, -- QQ
+    [YearMonth]         DATE            NOT NULL, -- End/Start of Month Date
+    [Month]             DATE            NOT NULL, -- 1900, Month, 1
+    [DayOfWeek]         DATE            NOT NULL, --1990, 1, Day
+
+    CONSTRAINT [PK_Dimension_Date] PRIMARY KEY CLUSTERED ( [DateID] ASC )
+)
--- NorthwindDW/Fact/Tables/Order.sql
+++ NorthwindDW/Fact/Tables/Order.sql
@@ -4,9 +4,9 @@
     [ProductID]                     INT             NOT NULL,
     [CustomerID]                    INT             NULL, 
     [EmployeeID]                    INT             NULL,
-    [OrderDateID]                   NVARCHAR(50)    NOT NULL, 
-    [RequiredDateID]                NVARCHAR(50)    NULL, 
-    [ShippedDateID]                 NVARCHAR(50)    NULL,
+    [OrderDateID]                   INT             NOT NULL, 
+    [RequiredDateID]                INT             NULL, 
+    [ShippedDateID]                 INT             NULL,
     [UnitPrice]                     MONEY           NULL,
     [Quantity]                      INT             NULL,
     [Discount]                      MONEY           NULL,
@@ -17,5 +17,8 @@
     
     CONSTRAINT [FK_Fact_Order_Customer_ID_Dimension_Customer] FOREIGN KEY ( [CustomerID] ) REFERENCES [Dimension].[Customer] ( [CustomerID] ),
     CONSTRAINT [FK_Fact_Order_Employee_ID_Dimension_Employee] FOREIGN KEY ( [EmployeeID] ) REFERENCES [Dimension].[Employee] ( [EmployeeID] ),
-    CONSTRAINT [FK_Fact_Order_Product_ID_Dimension_Product] FOREIGN KEY ( [ProductID] ) REFERENCES [Dimension].[Product] ( [ProductID] )
+    CONSTRAINT [FK_Fact_Order_Product_ID_Dimension_Product] FOREIGN KEY ( [ProductID] ) REFERENCES [Dimension].[Product] ( [ProductID] ),
+    CONSTRAINT [FK_Fact_Order_Order_Date_ID_Dimension_Date] FOREIGN KEY ( [OrderDateID] ) REFERENCES [Dimension].[Date] ( [DateID] ),
+    CONSTRAINT [FK_Fact_Order_Required_Date_ID_Dimension_Date] FOREIGN KEY ( [RequiredDateID] ) REFERENCES [Dimension].[Date] ( [DateID] ),
+    CONSTRAINT [FK_Fact_Order_Shipped_Date_ID_Dimension_Date] FOREIGN KEY ( [ShippedDateID] ) REFERENCES [Dimension].[Date] ( [DateID] )
 )
