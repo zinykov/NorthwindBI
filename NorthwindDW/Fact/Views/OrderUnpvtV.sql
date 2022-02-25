c7c27fd1b4aa46b834f4afe91033239addef589b
Денис Зинуков <zinykov@hotmail.com>
2022-02-26 00:40:48 +0300

    Added chema PowerBI for report Views. Added indexed Vews for Sales By Calendar and Unpivoted Order Fact Table.

---
+++ NorthwindDW/Fact/Views/OrderUnpvtV.sql
@@ -0,0 +1,58 @@
+﻿CREATE VIEW [Fact].[OrderUnpvtV] WITH SCHEMABINDING AS (
+	SELECT		  [OrderKey]
+				, [DateType]					=	CASE [DateType]
+														WHEN 'OrderDateKey'		THEN 'OrderDate'
+														WHEN 'RequiredDateKey'	THEN 'RequiredDate'
+														WHEN 'ShippedDateKey'	THEN 'ShippedDate'
+													END
+				, [DateKey]
+				, [ProductKey]
+				, [CustomerKey]
+				, [EmployeeKey]
+				, [UnitPrice]
+				, [Quantity]
+				, [Discount]
+				, [SalesAmount]
+				, [SalesAmountWithDiscount]
+	
+	FROM		(
+					SELECT		  [OrderKey]
+								, [ProductKey]
+								, [CustomerKey]
+								, [EmployeeKey]
+								, [OrderDateKey]
+								, [RequiredDateKey]
+								, [ShippedDateKey]
+								, [UnitPrice]
+								, [Quantity]
+								, [Discount]
+								, [SalesAmount]
+								, [SalesAmountWithDiscount]
+
+					FROM		[Fact].[Order]
+				) AS O
+
+	UNPIVOT		(
+					[DateKey] FOR [DateType] IN ( 
+						  [OrderDateKey]
+						, [RequiredDateKey]
+						, [ShippedDateKey]
+					)
+				) AS UNPVT
+);
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [UCIX_Fact_Order_Unpvt] ON [Fact].[OrderUnpvtV] ( [OrderKey] ASC, [DateType] ASC, [DateKey] ASC );
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Fact_Order_Unpvt_Date_Key] ON [Fact].[OrderUnpvtV] ( [DateKey] ASC );
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Fact_Order_Unpvt_Product_Key] ON [Fact].[OrderUnpvtV] ( [ProductKey] ASC );
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Fact_Order_Unpvt_Customer_Key] ON [Fact].[OrderUnpvtV] ( [CustomerKey] ASC );
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Fact_Order_Unpvt_Employee_Key] ON [Fact].[OrderUnpvtV] ( [EmployeeKey] ASC );
+GO
---
+++ NorthwindDW/PowerBI/Views/SalesByCalendarV.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW [PowerBI].[SalesByCalendarV] WITH SCHEMABINDING AS (
+	SELECT		  D.[Year]
+				, D.[Quarter]
+				, D.[Month]
+				, [UnitPrice]					=	SUM ( O.[UnitPrice] )
+				, [Quantity]					=	SUM ( O.[Quantity] )
+				, [Discount]					=	SUM ( O.[Discount] )
+				, [SalesAmount]					=	SUM ( O.[SalesAmount] )
+				, [SalesAmountWithDiscount]		=	SUM ( O.[SalesAmountWithDiscount] )
+				, [COUNT_BIG]					=	COUNT_BIG ( * )
+	
+	FROM		[Fact].[Order] AS O
+	INNER JOIN	[Dimension].[Date] AS D ON D.[DateKey] = O.[OrderDateKey]
+
+	GROUP BY	  D.[Year]
+				, D.[Quarter]
+				, D.[Month]
+	);
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [UCIX_PowerBI_By_Calender] ON [PowerBI].[SalesByCalendarV] ( [Year], [Quarter], [Month] );
+GO
