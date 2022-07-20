fbad2262bd2ab64dd8e2330945f97a60d0e8f05e
Денис Зинуков <zinykov@hotmail.com>
2022-07-21 01:03:45 +0300

    #100 Add paginated report

---
+++ NorthwindDW/Reports/Stored procedure/ssislog.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [Reports].[ssislog]
+	@period AS INT = 1
+AS
+	BEGIN
+		DECLARE @starttime AS DATETIME = DATEADD ( DAY, -1 * @period, GETDATE () )
+
+		SELECT		  [event]
+					, [operator]
+					, [source]
+					, [executionid]
+					, [starttime]
+					, [endtime]
+					, [datacode]
+					, [message]
+	
+		FROM		[dbo].[sysssislog]
+
+		WHERE		[starttime] >= @starttime
+	END
