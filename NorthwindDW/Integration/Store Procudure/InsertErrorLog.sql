920d5baede888d1523c9782bd94965044cb703ec
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-09-01 22:58:30 +0300

    Update solutions structure

---
+++ NorthwindDW/Integration/Store Procudure/InsertErrorLog.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [Integration].[InsertErrorLog]
+	  @ErrorCode INT
+	, @ErrorDescription NVARCHAR ( 2048 )
+	, @MachineName NVARCHAR ( 128 )
+	, @PackageName NVARCHAR ( 1024 )
+	, @SourceName NVARCHAR ( 1024 )
+	, @StartTime DATETIME2
+	, @UserName NVARCHAR ( 128 )
+	, @LineageKey INT
+AS BEGIN
+	INSERT INTO [Integration].[ErrorLog] (
+		  [ErrorCode]
+		, [ErrorDescription]
+		, [MachineName]
+		, [PackageName]
+		, [SourceName]
+		, [StartTime]
+		, [UserName]
+		, [LineageKey]
+	) VALUES (
+		  @ErrorCode
+		, @ErrorDescription
+		, @MachineName
+		, @PackageName
+		, @SourceName
+		, @StartTime
+		, @UserName
+		, @LineageKey
+	)
+END
--- NorthwindDW/Reports/Stored procedure/CountRowsInDWH.sql
+++ NorthwindDW/Reports/Stored procedure/CountRowsInDWH.sql
@@ -7,7 +7,7 @@ BEGIN
 	FROM		sys.tables AS T
 	INNER JOIN	sys.schemas AS S ON T.[schema_id] = S.[schema_id]
 	LEFT JOIN	sys.partitions AS P ON T.[object_id] = P.[object_id]
-				AND P.[index_id] = 1
+				AND P.[index_id] IN ( 1, 0 )
 	LEFT JOIN	sys.indexes AS I ON T.[object_id] = I.[object_id]
 				AND P.[index_id] = I.[index_id]
 	ORDER BY	  S.[schema_id]
