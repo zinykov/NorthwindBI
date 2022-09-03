8219ef376a5606cfe7feb52fc9460315d4e83526
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-09-03 10:58:35 +0300

    #117 Added landing views to manual DQS knowlage discovery and fix notification

---
+++ NorthwindDW/Landing/View/EmployeeV.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [Landing].[EmployeeV] AS
+	SELECT		  [EmployeeAlterKey]	=	E.[EmployeeID]
+				, [Name]				=	CONCAT ( E.[FirstName], ' ', E.[LastName] )
+				, [Title]				=	E.[Title]
+				, [TitleOfCourtesy]		=	E.[TitleOfCourtesy]
+				, [City]				=	E.[City]
+				, [Country]				=	E.[Country]
+
+	FROM		[Landing].[Employees] AS E
