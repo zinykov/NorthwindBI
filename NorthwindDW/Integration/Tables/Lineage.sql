cbb51fbcbb499103b15f0e82d5154cb1c5f5831f
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-09-16 00:02:02 +0300

    Add ExecutionId in LineAge table and ETL

--- NorthwindDW/Integration/Tables/Lineage.sql
+++ NorthwindDW/Integration/Tables/Lineage.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [Integration].[Lineage]
 (
-	  [LineageKey]					INT			CONSTRAINT [SQ_Lineage_Key] DEFAULT ( NEXT VALUE FOR [Sequences].[LineageKey] ) NOT NULL,
-	  [DataLoadStarted]				DATETIME2	NOT NULL,
-	  [TableName]					SYSNAME		NOT NULL,
-	  [DataLoadCompleted]			DATETIME2	NULL,
-	  [WasSuccessful]				BIT			NOT NULL,
-	  [CutoffTime]					DATETIME2	NOT NULL,
+	  [LineageKey]					INT				CONSTRAINT [SQ_Lineage_Key] DEFAULT ( NEXT VALUE FOR [Sequences].[LineageKey] ) NOT NULL,
+	  [DataLoadStarted]				DATETIME2		NOT NULL,
+	  [TableName]					SYSNAME			NOT NULL,
+	  [DataLoadCompleted]			DATETIME2		NULL,
+	  [WasSuccessful]				BIT				NOT NULL,
+	  [CutoffTime]					DATETIME2		NOT NULL,
+	  [ExecutionId]					NVARCHAR (50)	NOT NULL,
 	
 	CONSTRAINT [PK_Integration_Lineage] PRIMARY KEY CLUSTERED ( [LineageKey] ASC )
 )
