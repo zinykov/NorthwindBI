b23d3ecc5a77baeb9c735601a336519ea1c668a3
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-09-19 11:04:22 +0300

    #159

---
+++ NorthwindDW/Reports/Stored procedure/DQS_status.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [Reports].[DQS_status] AS
+BEGIN
+	SELECT		  [Table]			= 'Customer'
+				, [Record_Status]
+				, [Row Count]		= COUNT ( * )
+
+	FROM		[DQS_STAGING_DATA].[dbo].[Customer]
+
+	GROUP BY	[Record_Status]
+
+	UNION ALL
+
+	SELECT		  [Table]			= 'Employee'
+				, [Record_Status]
+				, [Row Count]		= COUNT ( * )
+
+	FROM		[DQS_STAGING_DATA].[dbo].[Employee]
+
+	GROUP BY	[Record_Status]
+
+	UNION ALL
+
+	SELECT		  [Table]			= 'Product'
+				, [Record_Status]
+				, [Row Count]		= COUNT ( * )
+
+	FROM		[DQS_STAGING_DATA].[dbo].[Product]
+
+	GROUP BY	[Record_Status]
+END
