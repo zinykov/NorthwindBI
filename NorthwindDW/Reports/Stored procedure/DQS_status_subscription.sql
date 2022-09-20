5577bc4c93d30614797cea4692e37450c4e0872b
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-09-20 23:25:14 +0300

    #160

---
+++ NorthwindDW/Reports/Stored procedure/DQS_status_subscription.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [Reports].[DQS_status_subscription] AS
+BEGIN
+	IF OBJECT_ID ( N'#tempDQSstatus' ) IS NOT NULL
+		BEGIN
+			DROP TABLE #tempDQSstatus
+		END
+			
+	CREATE TABLE #tempDQSstatus (
+		  [Table] NVARCHAR(50)
+		, [Record_Status] NVARCHAR(50)
+		, [Row Count] INT
+	)
+	
+	INSERT #tempDQSstatus EXECUTE [Reports].[DQS_status];
+		
+	IF EXISTS ( SELECT 1 FROM #tempDQSstatus WHERE [Record_Status] NOT IN ( 'Correct' ) )
+		BEGIN
+			SELECT	  [Кому]				=	'zinykov@hotmail.com'
+					, [Копия]				=	NULL
+					, [СК]					=	NULL
+					, [Обратный адрес]		=	NULL
+					, [Включить отчет]		=	CAST ( 1 AS bit )
+					, [Формат отображения]	=	'MHTML'
+					, [Приоритет]			=	'Normal'
+					, [Тема]				=	'DQS Cleansing projects status'
+					, [Комментарий]			=	NULL
+					, [Включить ссылку]		=	CAST ( 1 AS bit )
+		END
+			
+	DROP TABLE #tempDQSstatus;
+END
