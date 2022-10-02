1c63aeb626d73bceefca2fbd100f3bc2b6c027c6
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-10-03 00:14:16 +0300

    #167 #118 Fix Load Employee dtsx

---
+++ NorthwindDW/Integration/Store Procudure/UpdateProduct.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROC [Integration].[UpdateProductChangingAttributes]
+	  @Product AS NVARCHAR(50)
+	, @Category AS NVARCHAR(50)
+	, @LineageKey AS INT
+	, @ProductAlterKey AS INT
+AS
+BEGIN
+	UPDATE [Dimension].[Product] SET [Product] = @Product, [Category] = @Category, [LineageKey] = @LineageKey WHERE [ProductAlterKey] = @ProductAlterKey
+END
--- NorthwindDW/Staging/Tables/Product.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [Staging].[Product]
-(
-	[ProductAlterKey]       INT                                 NOT NULL,
-    [Product]               NVARCHAR(50)                        NOT NULL, 
-    [Category]              NVARCHAR(50)                        NOT NULL
-)
-    ON [Dimention_Data]
-    WITH ( DATA_COMPRESSION = PAGE );
-GO
