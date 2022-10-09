900207ae73ccba4d516c602f742ef33af5cf5f65
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-10-09 18:03:20 +0300

    #171
    Связанные рабочие элементы: #118; #171

---
+++ DQS_STAGING_DATA/dbo/Tables/NW_Category.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[NW_Category] (
+    [CategoryID_Source]       INT            NULL,
+    [CategoryID_Output]       INT            NULL,
+    [CategoryID_Reason]       NVARCHAR (MAX) NULL,
+    [CategoryID_Confidence]   NVARCHAR (MAX) NULL,
+    [CategoryID_Status]       NVARCHAR (MAX) NULL,
+    [CategoryName_Source]     NVARCHAR (15)  NULL,
+    [CategoryName_Output]     NVARCHAR (MAX) NULL,
+    [CategoryName_Reason]     NVARCHAR (MAX) NULL,
+    [CategoryName_Confidence] NVARCHAR (MAX) NULL,
+    [CategoryName_Status]     NVARCHAR (MAX) NULL,
+    [Description_Output]      NVARCHAR (MAX) NULL,
+    [Record_Status]           NVARCHAR (MAX) NULL
+);
+
---
+++ DQS_STAGING_DATA/dbo/Tables/NW_Product.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[NW_Product] (
+    [ProductID_Source]       INT            NULL,
+    [ProductID_Output]       INT            NULL,
+    [ProductID_Reason]       NVARCHAR (MAX) NULL,
+    [ProductID_Confidence]   NVARCHAR (MAX) NULL,
+    [ProductID_Status]       NVARCHAR (MAX) NULL,
+    [ProductName_Source]     NVARCHAR (40)  NULL,
+    [ProductName_Output]     NVARCHAR (MAX) NULL,
+    [ProductName_Reason]     NVARCHAR (MAX) NULL,
+    [ProductName_Confidence] NVARCHAR (MAX) NULL,
+    [ProductName_Status]     NVARCHAR (MAX) NULL,
+    [SupplierID_Output]      INT            NULL,
+    [CategoryID_Source]      INT            NULL,
+    [CategoryID_Output]      INT            NULL,
+    [CategoryID_Reason]      NVARCHAR (MAX) NULL,
+    [CategoryID_Confidence]  NVARCHAR (MAX) NULL,
+    [CategoryID_Status]      NVARCHAR (MAX) NULL,
+    [UnitPrice_Output]       MONEY          NULL,
+    [Record_Status]          NVARCHAR (MAX) NULL
+);
+
