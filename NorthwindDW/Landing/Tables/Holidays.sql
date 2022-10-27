ab9320fe5a9ca481e9e48088effde9543c3e2e95
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2022-10-27 14:45:30 +0300

    #176

---
+++ NorthwindDW/Landing/Tables/Holidays.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Landing].[Holidays]
+(
+	[Date]			NVARCHAR(255)	NOT NULL,
+	[DateType]		NVARCHAR(255)	NOT NULL,
+	[HolidayName]	NVARCHAR(255)	NULL
+)
+    ON [Landing_FG]
+    WITH ( DATA_COMPRESSION = PAGE ) ;
+GO
