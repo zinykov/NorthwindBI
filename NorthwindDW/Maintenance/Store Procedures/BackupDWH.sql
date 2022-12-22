aa30c2da7eff5f459be6514d6109406c669c995c
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2022-12-22 22:18:21 +0700

    Fixed SP

--- NorthwindDW/Maintenance/Store Procedures/BackupDWH.sql
+++ NorthwindDW/Maintenance/Store Procedures/BackupDWH.sql
@@ -9,18 +9,28 @@ AS
 */
 BEGIN
 	--DECLARE @CutoffTime		    AS DATE;
+	DECLARE @BackupSQL		    AS NVARCHAR(4000);
+	DECLARE @BackupDevice	    AS NVARCHAR(2000);
+	
 	DECLARE @ReferenceDate      AS DATE;
     DECLARE @StartYearDate      AS DATE;
 	DECLARE @EndYearDate	    AS DATE;
 	DECLARE @StartKey		    AS INT;
 	DECLARE @EndKey			    AS INT;
-	DECLARE @Bondaries		    AS NVARCHAR(2000);
-	DECLARE @CreatePF		    AS NVARCHAR(4000);
+	
 	DECLARE @CreatePS		    AS NVARCHAR(4000);
     DECLARE @PartitionNumber    AS INT;
     DECLARE @PartitionValue     AS INT;
     
 	--SET @CutoffTime = DATEFROMPARTS ( 1998, 5, 3 )
 
+	SET @BackupSQL = CONCAT ( 
+		  N'BACKUP DATABASE ['
+		, [$(DatabaseName)]
+		, N'] ON DISK = '''
+		, @BackupDevice
+		, ''' WITH FORMAT, INIT, COMPRESSION'
+	)
 
+	--IF 
 END
