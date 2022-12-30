874c207dfb26f593546bd3f365b20bb6a98be0db
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2022-12-30 20:52:11 +0700

    #224

--- NorthwindDW/Maintenance/Store Procedures/SetFilegroupReadOnly.sql
+++ NorthwindDW/Maintenance/Store Procedures/SetFilegroupReadOnly.sql
@@ -8,37 +8,18 @@ AS BEGIN
 	DECLARE @FileGroupIndexName AS NVARCHAR(200);
     DECLARE @SQL AS NVARCHAR(2000);
 
-	SET @CutoffTimeYear = YEAR ( @CutoffTime ) - 2
-    SET @SQL = CONCAT (
-          N'ALTER INDEX [CCI_Fact_Order] ON [Fact].[Order] REBUILD PARTITION = $PARTITION.[PF_Order_Date] ( '
-        , @CutoffTimeYear * 10000 + 101
-        , N' ) WITH ( DATA_COMPRESSION = COLUMNSTORE_ARCHIVE );'
-    )
 	SET @FileGroupDataName = CONCAT ( @FactTableName, N'_', @CutoffTimeYear, '_Data' )
 	SET @FileGroupIndexName = CONCAT ( @FactTableName, N'_', @CutoffTimeYear, '_Index' )
 
-	EXECUTE sp_executesql @SQL
     
     IF EXISTS ( SELECT 1 FROM sys.filegroups WHERE [name] = @FileGroupDataName AND [is_read_only] = 0 )
 		BEGIN
-            --SELECT @SQL = CONCAT ( @SQL, N'KILL ', CONVERT(varchar(5), session_id), N'; ' )
-			--FROM sys.dm_exec_sessions
-			--WHERE database_id  = db_id('$(DatabaseName)')
-
-			--EXECUTE sp_executesql @SQL
-
-			SET @SQL = CONCAT ( N'ALTER DATABASE [NorthwindDW] MODIFY FILEGROUP [', @FileGroupDataName, N'] READ_ONLY;' )
+            SET @SQL = CONCAT ( N'ALTER DATABASE [NorthwindDW] MODIFY FILEGROUP [', @FileGroupDataName, N'] READ_ONLY;' )
 
 			EXECUTE sp_executesql @SQL
 		END
-	IF EXISTS ( SELECT 1 FROM sys.filegroups WHERE [name] = @FileGroupDataName AND [is_read_only] = 0 )
+	IF EXISTS ( SELECT 1 FROM sys.filegroups WHERE [name] = @FileGroupIndexName AND [is_read_only] = 0 )
 		BEGIN
-			--SELECT @SQL = CONCAT ( @SQL, N'KILL ', CONVERT(varchar(5), [session_id] ), N'; ' )
-			--FROM sys.dm_exec_sessions
-			--WHERE [database_id]  = DB_ID ( '$(DatabaseName)' )
-
-			--EXECUTE sp_executesql @SQL
-
 			SET @SQL = CONCAT ( N'ALTER DATABASE [NorthwindDW] MODIFY FILEGROUP [', @FileGroupIndexName, N'] READ_ONLY;' )
 
 			EXECUTE sp_executesql @SQL
---
+++ NorthwindDW/Scripts/SetFilegroupsReadOnly.sql
@@ -0,0 +1,6 @@
+﻿ALTER DATABASE [$(DatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+EXECUTE [Maintenance].[SetFilegroupReadOnly] @CutoffTime = $(CutoffTime), @FactTableName = $(FactTableName)
+GO
+ALTER DATABASE [$(DatabaseName)] SET MULTI_USER;
+GO
