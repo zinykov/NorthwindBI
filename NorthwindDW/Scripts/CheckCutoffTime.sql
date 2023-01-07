8484f16144a362eea52b6d2b1a2e53559180d64a
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-01-07 15:40:35 +0700

    #224

---
+++ NorthwindDW/Scripts/CheckCutoffTime.sql
@@ -0,0 +1,8 @@
+﻿--:setvar Cutofftime '1998-01-01'
+
+SELECT	[AlterDateKey]
+FROM	[Dimension].[Date]
+WHERE	[DayOfWeekNumber] = 6
+		AND [DayOfMonth] BETWEEN 2 AND 8
+		AND [MonthNumber] = 1
+        AND [Year] = YEAR ( $(Cutofftime) )
--- NorthwindDW/Scripts/SetFilegroupsReadOnly.sql
+++ NorthwindDW/Scripts/SetFilegroupsReadOnly.sql
@@ -1,6 +1,106 @@
-﻿ALTER DATABASE [$(DatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+﻿--:setvar DatabaseName NorthwindDW
+--:setvar Cutofftime '1998-01-01'
+--:setvar FactTableName Order
+	
+--	DECLARE @ReferenceDate AS DATE = (
+--        SELECT	[AlterDateKey]
+--        FROM	[Dimension].[Date]
+--        WHERE	[DayOfWeekNumber] = 6
+--			    AND [DayOfMonth] BETWEEN 2 AND 8
+--			    AND [MonthNumber] = 1
+--                AND [Year] = YEAR ( $(Cutofftime) )
+--    )
+
+--    IF @CutoffTime <> @ReferenceDate OR @CutoffTime = DATEFROMPARTS ( 1997, 1, 4 ) ;
+
+USE [$(DatabaseName)];
 GO
-EXECUTE [Maintenance].[SetFilegroupReadOnly] @CutoffTime = $(CutoffTime), @FactTableName = $(FactTableName)
+
+ALTER DATABASE [$(DatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 GO
-ALTER DATABASE [$(DatabaseName)] SET MULTI_USER;
+
+DECLARE @FilegroupName AS NVARCHAR(100) = CONCAT (
+	  N'$(FactTableName)_'
+	, CONVERT ( INT, LEFT ( $(Cutofftime), 4 ) - 2 )
+	, N'_Data'
+)
+DECLARE @ReadOnly AS BIT
+DECLARE @SQL AS NVARCHAR(1000) = CONCAT (
+	  N'ALTER DATABASE [$(DatabaseName)] MODIFY FILEGROUP ['
+	, @FilegroupName
+	, N'] READONLY'
+)
+
+SELECT @readonly = CONVERT ( BIT, ( STATUS & 0x08 ) ) FROM sysfilegroups WHERE groupname = @FilegroupName
+IF ( @readonly = 0 )
+	BEGIN
+		EXECUTE sp_executesql @SQL;
+		PRINT CONCAT ( N'@readonly = ', @readonly, N' @SQL = ', @SQL )
+	END;
+GO
+
+DECLARE @FilegroupName AS NVARCHAR(100) = CONCAT (
+	  N'$(FactTableName)_'
+	, CONVERT ( INT, LEFT ( $(Cutofftime), 4 ) - 2 )
+	, N'_Index'
+)
+DECLARE @ReadOnly AS BIT
+DECLARE @SQL AS NVARCHAR(1000) = CONCAT (
+	  N'ALTER DATABASE [$(DatabaseName)] MODIFY FILEGROUP ['
+	, @FilegroupName
+	, N'] READONLY'
+)
+
+SELECT @readonly = CONVERT ( BIT, ( STATUS & 0x08 ) ) FROM sysfilegroups WHERE groupname = @FilegroupName
+IF ( @readonly = 0 )
+	BEGIN
+		EXECUTE sp_executesql @SQL;
+		PRINT CONCAT ( N'@readonly = ', @readonly, N' @SQL = ', @SQL )
+	END;
+GO
+
+USE [master];
+GO
+
+DECLARE @FilegroupName AS NVARCHAR(100) = CONCAT (
+	  N'$(FactTableName)_'
+	, CONVERT ( INT, LEFT ( $(Cutofftime), 4 ) - 2 )
+	, N'_Data'
+)
+DECLARE @ReadOnly AS BIT
+DECLARE @SQL AS NVARCHAR(1000) = CONCAT (
+	  N'ALTER DATABASE [$(DatabaseName)] MODIFY FILEGROUP ['
+	, @FilegroupName
+	, N'] READONLY'
+)
+
+SELECT @readonly = CONVERT ( BIT, ( STATUS & 0x08 ) ) FROM sysfilegroups WHERE groupname = @FilegroupName
+IF ( @readonly = 0 )
+	BEGIN
+		EXECUTE sp_executesql @SQL;
+		PRINT CONCAT ( N'@readonly = ', @readonly, N' @SQL = ', @SQL )
+	END;
+GO
+
+DECLARE @FilegroupName AS NVARCHAR(100) = CONCAT (
+	  N'$(FactTableName)_'
+	, CONVERT ( INT, LEFT ( $(Cutofftime), 4 ) - 2 )
+	, N'_Index'
+)
+DECLARE @ReadOnly AS BIT
+DECLARE @SQL AS NVARCHAR(1000) = CONCAT (
+	  N'ALTER DATABASE [$(DatabaseName)] MODIFY FILEGROUP ['
+	, @FilegroupName
+	, N'] READONLY'
+)
+
+SELECT @readonly = CONVERT ( BIT, ( STATUS & 0x08 ) ) FROM sysfilegroups WHERE groupname = @FilegroupName
+IF ( @readonly = 0 )
+	BEGIN
+		EXECUTE sp_executesql @SQL;
+		PRINT CONCAT ( N'@readonly = ', @readonly, N' @SQL = ', @SQL )
+	END;
+GO
+
+ALTER DATABASE [$(DatabaseName)] SET MULTI_USER WITH ROLLBACK IMMEDIATE;
 GO
