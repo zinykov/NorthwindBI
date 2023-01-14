5e4ccec183dc50a502f7668e6ed015f945ca88dd
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-01-14 22:03:29 +0700

    #224

--- NorthwindDW/Scripts/CheckCutoffTime.sql
+++
@@ -1,8 +0,0 @@
-﻿:setvar Cutofftime '1998-01-31'
-
-SELECT	[AlterDateKey]
-FROM	[Dimension].[Date]
-WHERE	[DayOfWeekNumber] = 6
-		AND [DayOfMonth] BETWEEN 2 AND 8
-		AND [MonthNumber] = 1
-        AND [Year] = YEAR ( $(Cutofftime) )
