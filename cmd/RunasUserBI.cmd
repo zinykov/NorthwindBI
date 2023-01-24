d9a47ed45e3057fd106e7ff5179aafdbc0a9eb50
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-01-24 20:50:10 +0700

    Renamed cmd script

---
+++ cmd/RunasUserBI.cmd
@@ -0,0 +1,3 @@
+Runas /user:UserBI "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\SSMS.exe"
+
+Runas /user:UserBI "C:\Program Files\WindowsApps\Microsoft.MicrosoftPowerBIDesktop_2.112.1161.0_x64__8wekyb3d8bbwe\bin\PBIDesktop.exe"
--- cmd/SSMSUserBI.cmd
+++
@@ -1,1 +0,0 @@
-Runas /user:UserBI "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\SSMS.exe"
