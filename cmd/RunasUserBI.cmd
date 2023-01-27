998bcb8df464cba595c3185a585ecd63660ee87b
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-01-27 13:21:52 +0700

    Minor code refuctoring

--- cmd/RunasUserBI.cmd
+++ cmd/RunasUserBI.cmd
@@ -1,3 +1,1 @@
 Runas /user:UserBI "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\SSMS.exe"
-
-Runas /user:UserBI "C:\Program Files\WindowsApps\Microsoft.MicrosoftPowerBIDesktop_2.112.1161.0_x64__8wekyb3d8bbwe\bin\PBIDesktop.exe"
