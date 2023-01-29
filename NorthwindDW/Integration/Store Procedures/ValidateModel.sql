1a38f5719209ea9d7db1fb0fc090801af3fd21bf
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-01-30 00:30:06 +0700

    #93 Fixed macro vars

--- NorthwindDW/Integration/Store Procedures/ValidateModel.sql
+++ NorthwindDW/Integration/Store Procedures/ValidateModel.sql
@@ -6,9 +6,9 @@ AS BEGIN
     DECLARE @User_ID INT
     DECLARE @Version_ID INT
 
-    SET @User_ID    = ( SELECT U.[ID] FROM [$(MDS_ServerName)].[$(MDS_DatabaseName)].[mdm].[tblUser] AS U WHERE U.[UserName] = @UserName )
-    SET @Model_ID   = ( SELECT TOP 1 [Model_ID] FROM [$(MDS_ServerName)].[$(MDS_DatabaseName)].[mdm].[viw_SYSTEM_SCHEMA_VERSION] WHERE [Model_Name] = @ModelName )
-    SET @Version_ID = ( SELECT MAX ( [ID] ) FROM [$(MDS_ServerName)].[$(MDS_DatabaseName)].[mdm].[viw_SYSTEM_SCHEMA_VERSION] WHERE [Model_ID] = @Model_ID )
+    SET @User_ID    = ( SELECT U.[ID] FROM [$(MDSServerName)].[$(MDSDatabaseName)].[mdm].[tblUser] AS U WHERE U.[UserName] = @UserName )
+    SET @Model_ID   = ( SELECT TOP 1 [Model_ID] FROM [$(MDSServerName)].[$(MDSDatabaseName)].[mdm].[viw_SYSTEM_SCHEMA_VERSION] WHERE [Model_Name] = @ModelName )
+    SET @Version_ID = ( SELECT MAX ( [ID] ) FROM [$(MDSServerName)].[$(MDSDatabaseName)].[mdm].[viw_SYSTEM_SCHEMA_VERSION] WHERE [Model_ID] = @Model_ID )
   
     EXECUTE [$(MDS_ServerName)].[$(MDS_DatabaseName)].[mdm].[udpValidateModel]
           @User_ID
