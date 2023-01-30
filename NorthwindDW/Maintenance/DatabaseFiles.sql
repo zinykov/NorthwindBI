145200e020977a56ea2a702b842c5f9415c4db3e
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-01-30 23:07:00 +0700

    #228

---
+++ NorthwindDW/Maintenance/DatabaseFiles.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [Maintenance].[DatabaseFiles] (
+	[DatabaseFileKey]					INT				CONSTRAINT [SQ_DatabaseFile_Key] DEFAULT ( NEXT VALUE FOR [Sequences].[DatabaseFileKey] ) NOT NULL,
+	[FilegroupName]						NVARCHAR(100)	NOT NULL,
+	[FilePath]							NVARCHAR(500)	NOT NULL,
+
+	CONSTRAINT [PK_Maintenance_DatabaseFiles] PRIMARY KEY CLUSTERED ( [DatabaseFileKey] ASC )
+)
+    ON [Dimention_Data]
+    WITH ( DATA_COMPRESSION = PAGE ) ;
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Maintenance_DatabaseFiles_FilegroupName] ON [Maintenance].[DatabaseFiles] ( [FilegroupName] ASC )
+    WITH ( DATA_COMPRESSION = PAGE )
+    ON [Dimention_Index];
+GO
