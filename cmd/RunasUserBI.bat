bf5fbbe38608929304878c06a69c580a526d3ce0
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-02-15 22:06:38 +0700

    #232

--- cmd/RunasUserBI.bat
+++ cmd/RunasUserBI.bat
@@ -1,1 +1,3 @@
-Runas /user:RDLexec "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\Ssms.exe"
+RunAs /user:UserBI "\"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe\" http://swift3/reports/browse/Monitoring"
+
+::RunAs /user:RDLexec "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\Ssms.exe"
