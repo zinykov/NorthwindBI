b0413d169e439bc8c1c4fb149fc3fab4d113b2dd
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-02-17 14:29:50 +0700

    #231

---
+++ NorthwindDW/Scripts/DropAndRestore.sql
@@ -0,0 +1,18 @@
+﻿--:setvar DWHDatabaseName "NorthwindDW"
+--:setvar ExternalFilesPath "C:\Users\zinyk\source\repos\Northwind_BI_Solution\NorthwindDW\"
+
+USE [master];
+GO
+
+ALTER DATABASE [$(DWHDatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+PRINT N'[$(DWHDatabaseName)] setted in SINGLE_USER mode';
+GO
+
+IF EXISTS ( SELECT 1 FROM [sys].[databases] WHERE [name] = N'$(DWHDatabaseName)' )
+	BEGIN
+		DROP DATABASE [$(DWHDatabaseName)]
+		PRINT N'[$(DWHDatabaseName)] dropped'
+	END;
+GO
+
+:r $(ExternalFilesPath)\Scripts\RestoreDatabase.sql
