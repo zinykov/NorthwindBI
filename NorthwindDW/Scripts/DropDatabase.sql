3068015870b9ed70dd00b5866117174b8e602d77
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-02-27 08:41:24 +0700

    #245 Fixed

--- NorthwindDW/Scripts/DropDatabase.sql
+++ NorthwindDW/Scripts/DropDatabase.sql
@@ -1,16 +1,15 @@
-﻿--:setvar DWHDatabaseName "NorthwindDW"
---:setvar ExternalFilesPath "C:\Users\zinyk\source\repos\Northwind_BI_Solution\NorthwindDW\"
+﻿--:setvar DWHDatabaseName NorthwindDW
 
 USE [master];
 GO
 
 ALTER DATABASE [$(DWHDatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-PRINT N'[$(DWHDatabaseName)] setted in SINGLE_USER mode';
+PRINT N'$(DWHDatabaseName) setted in SINGLE_USER mode';
 GO
 
 IF EXISTS ( SELECT 1 FROM [sys].[databases] WHERE [name] = N'$(DWHDatabaseName)' )
 	BEGIN
 		DROP DATABASE [$(DWHDatabaseName)]
-		PRINT N'[$(DWHDatabaseName)] dropped'
+		PRINT N'$(DWHDatabaseName) dropped'
 	END;
 GO
--- NorthwindDW/Scripts/RestoreDatabase.sql
+++ NorthwindDW/Scripts/RestoreDatabase.sql
@@ -1,4 +1,4 @@
-﻿--:setvar DWHDatabaseName NorthwindDW
+﻿:setvar DWHDatabaseName NorthwindDW
 
 USE [Logs];
 GO
@@ -8,62 +8,64 @@ DECLARE @BackupFileName AS NVARCHAR(500);
 DECLARE @SQL AS NVARCHAR(1000);
 DECLARE @GroupName AS NVARCHAR(100);
 
-DECLARE RestoreDWH CURSOR FOR 
-	SELECT		  DISTINCT [IsReadOnly], [BackupFileName]
+SET @BackupFileName = (
+		SELECT		TOP(1) [BackupFileName]
+		FROM		[dbo].[DatabaseFiles]
+		WHERE		  [BackupFileName] LIKE '%_FULL.bak'
+		ORDER BY	  [BackupFileName] DESC
+)	
+SET @SQL = CONCAT (
+	  N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] READ_WRITE_FILEGROUPS FROM DISK = '''
+	, @BackupFileName
+	, N''' WITH PARTIAL, NORECOVERY;'
+)
+
+EXECUTE sp_executesql @SQL
+						
+SET @BackupFileName = (
+	SELECT		TOP(1) [BackupFileName]
 	FROM		[dbo].[DatabaseFiles]
-	ORDER BY	  [IsReadOnly] ASC
-				, [BackupFileName] ASC
+	WHERE		  [BackupFileName] LIKE '%_DIFF.bak' 
+	ORDER BY	  [BackupFileName] DESC
+)
+SET @SQL = CONCAT (
+	  N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] READ_WRITE_FILEGROUPS FROM DISK = '''
+	, @BackupFileName
+	, N''' WITH NORECOVERY;'
+)
+
+EXECUTE sp_executesql @SQL
+			
+SET @SQL = N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] WITH RECOVERY;'
+EXECUTE sp_executesql @SQL
+
+DECLARE RestoreDWH CURSOR FOR 
+	SELECT		  [BackupFileName]
+	FROM		[dbo].[DatabaseFiles]
+	WHERE		  [IsReadOnly] = 1
+	ORDER BY	  [BackupFileName] ASC
 
 OPEN RestoreDWH
-	FETCH NEXT FROM RestoreDWH INTO @IsReadOnly, @BackupFileName
+	FETCH NEXT FROM RestoreDWH INTO @BackupFileName
 	WHILE @@FETCH_STATUS = 0
 		BEGIN
-			IF ( @IsReadOnly = 0 )
-				BEGIN
-					IF ( @BackupFileName LIKE '%_FULL.bak' )
-						BEGIN
-							SET @SQL = CONCAT (
-								  N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] READ_WRITE_FILEGROUPS FROM DISK = '''
-								, @BackupFileName
-								, N''' WITH PARTIAL, NORECOVERY;'
-							)
+			SET @GroupName = (
+				SELECT	[GroupName]
+				FROM	[dbo].[DatabaseFiles]
+				WHERE	[BackupFileName] = @BackupFileName
+			)
 
-							EXECUTE sp_executesql @SQL
-						END
-					ELSE IF ( @BackupFileName LIKE '%_DIFF.bak' )
-						BEGIN
-							SET @SQL = CONCAT (
-								  N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] READ_WRITE_FILEGROUPS FROM DISK = '''
-								, @BackupFileName
-								, N''' WITH NORECOVERY;'
-							)
+			SET @SQL = CONCAT (
+					N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] FILEGROUP = '''
+				, @GroupName
+				, N''' FROM DISK = '''
+				, @BackupFileName
+				, N''' WITH RECOVERY;'
+			)
 
-							EXECUTE sp_executesql @SQL
-						END
-				END
-			
-			SET @SQL = N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] WITH RECOVERY;'
 			EXECUTE sp_executesql @SQL
-
-			IF ( @IsReadOnly = 1 )
-				BEGIN
-					SET @GroupName = (
-						SELECT	[GroupName]
-						FROM	[dbo].[DatabaseFiles]
-						WHERE	[BackupFileName] = @BackupFileName
-					)
-
-					SET @SQL = CONCAT (
-						  N'USE [master]; RESTORE DATABASE [$(DWHDatabaseName)] FILEGROUP = '''
-						, @GroupName
-						, N''' FROM DISK = '''
-						, @BackupFileName
-						, N''' WITH RECOVERY;'
-					)
-
-					EXECUTE sp_executesql @SQL
-				END
-			FETCH NEXT FROM RestoreDWH INTO @IsReadOnly, @BackupFileName
+			
+			FETCH NEXT FROM RestoreDWH INTO @BackupFileName
 		END
 CLOSE RestoreDWH
 DEALLOCATE RestoreDWH
