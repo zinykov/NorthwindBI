b2dea8502778faa194ac9d360dea9622d9cb1ba7
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-02-28 01:07:57 +0700

    #241

--- NorthwindDW/Maintenance/Store Procedures/GetOrInsertDatabaseFilesData.sql
+++ NorthwindDW/Maintenance/Store Procedures/GetOrInsertDatabaseFilesData.sql
@@ -2,37 +2,37 @@
 	  @CutoffTime AS DATE
 	, @FactTableName AS NVARCHAR(100)
 	, @FilePath AS NVARCHAR(500)
-
 AS BEGIN
 	DECLARE @GroupName AS NVARCHAR(100)
 	DECLARE @Year AS INT
 	DECLARE @Name AS NVARCHAR(100)
 	DECLARE @FileName AS NVARCHAR(500)
 
-	SET @GroupName = CONCAT ( @FactTableName, N'_', @Year )
+	SET @GroupName = CONCAT ( @FactTableName, N'_', YEAR ( DATEADD ( DAY, 1, @CutoffTime ) ) )
 
 	IF NOT EXISTS (
 		SELECT		1
 		FROM		[Maintenance].[DatabaseFiles] AS DBF
 		INNER JOIN	sys.sysfilegroups AS FG ON FG.[groupname] = DBF.[GroupName]
 		WHERE		DBF.[GroupName] = CONCAT ( @GroupName, N'_Data' )
-	)
+	) BEGIN
 		EXECUTE [Maintenance].[InsertDatabaseFilesData]
 			  @CutoffTime = @CutoffTime
 			, @FactTableName = @FactTableName
 			, @FilePath = @FilePath
 			, @IsClustered = 1
-	
+	END
+
 	IF NOT EXISTS (
-		SELECT		1
+		SELECT		*
 		FROM		[Maintenance].[DatabaseFiles] AS DBF
 		INNER JOIN	sys.sysfilegroups AS FG ON FG.[groupname] = DBF.[GroupName]
 		WHERE		DBF.[GroupName] = CONCAT ( @GroupName, N'_Index' )
-	)		
+	) BEGIN
 		EXECUTE [Maintenance].[InsertDatabaseFilesData]
 			  @CutoffTime = @CutoffTime
 			, @FactTableName = @FactTableName
 			, @FilePath = @FilePath
 			, @IsClustered = 0
-
+	END
 END
