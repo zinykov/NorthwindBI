d235a7d8c0057757c99e673c1e7379cd4ebfeb14
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-02-28 13:45:19 +0700

    #243

--- NorthwindDW/Scripts/ResourceGovernor.sql
+++ NorthwindDW/Scripts/ResourceGovernor.sql
@@ -5,10 +5,20 @@ IF NOT EXISTS ( SELECT 1 FROM sys.resource_governor_resource_pools WHERE [name] 
         WITH (
               min_cpu_percent       = 20
             , max_cpu_percent       = 90
-            , min_memory_percent    = 50
+            , min_memory_percent    = 20
             , max_memory_percent    = 90
-            , min_iops_per_volume   = 50
-            , max_iops_per_volume   = 100
+            , min_iops_per_volume   = 65536
+            , max_iops_per_volume   = 0
+        );
+ELSE
+    ALTER RESOURCE POOL [High Priority]
+        WITH (
+              min_cpu_percent       = 20
+            , max_cpu_percent       = 90
+            , min_memory_percent    = 20
+            , max_memory_percent    = 90
+            , min_iops_per_volume   = 65536
+            , max_iops_per_volume   = 0
         );
 GO
 
@@ -20,7 +30,17 @@ IF NOT EXISTS ( SELECT 1 FROM sys.resource_governor_resource_pools WHERE [name] 
             , min_memory_percent    = 0
             , max_memory_percent    = 50
             , min_iops_per_volume   = 0
-            , max_iops_per_volume   = 20
+            , max_iops_per_volume   = 65536
+        );
+ELSE
+    ALTER RESOURCE POOL [Low Priority]
+        WITH (
+              min_cpu_percent       = 0
+            , max_cpu_percent       = 50
+            , min_memory_percent    = 0
+            , max_memory_percent    = 50
+            , min_iops_per_volume   = 0
+            , max_iops_per_volume   = 65536
         );
 GO
 
@@ -35,10 +55,32 @@ IF NOT EXISTS ( SELECT 1 FROM sys.resource_governor_workload_groups WHERE [name]
 		    , max_dop                           = 1
 	    )
 	    USING [Low Priority];
+ELSE
+    ALTER WORKLOAD GROUP [User Queries]
+	    WITH (
+		      group_max_requests                = 10
+		    , importance                        = Low
+		    , request_max_cpu_time_sec          = 50
+		    , request_max_memory_grant_percent  = 50
+		    , request_memory_grant_timeout_sec  = 20
+		    , max_dop                           = 1
+	    )
+	    USING [Low Priority];
 GO
 
 IF NOT EXISTS ( SELECT 1 FROM sys.resource_governor_workload_groups WHERE [name] = N'ETL' )
     CREATE WORKLOAD GROUP [ETL]
+        WITH (
+              group_max_requests                = 100
+            , importance                        = High
+            , request_max_cpu_time_sec          = 80
+            , request_max_memory_grant_percent  = 80
+            , request_memory_grant_timeout_sec  = 30
+            , max_dop                           = 4
+        )
+        USING [High Priority];
+ELSE
+    ALTER WORKLOAD GROUP [ETL]
         WITH (
               group_max_requests                = 100
             , importance                        = High
