31bd4e4cd7bdcf4e063cd5b6fa8948ba6e8bfb05
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-03-28 14:49:17 +0700

    #299 #301

--- Landing/Landing/Store Procedures/ExtractCategories.sql
+++ Landing/Landing/Store Procedures/ExtractCategories.sql
@@ -1,16 +1,29 @@
 ﻿CREATE PROCEDURE [Landing].[ExtractCategories] AS
 BEGIN
+	UPDATE [Landing].[Categories]
+	SET [CheckSum] = CHECKSUM ( [CategoryID], [CategoryName] );
+
+	CREATE INDEX [IX_Landing_Categories_Hash] ON [Landing].[Categories] ( [CheckSum] )
+		INCLUDE ( [CategoryID], [CategoryName] );
+	
 	SELECT		  LC.[CategoryID]
 				, LC.[CategoryName]
 	FROM		[Landing].[Categories] AS LC
 	LEFT JOIN	[Hash].[Categories] AS HC ON LC.[CategoryID] = HC.[CategoryID]
-	WHERE		CHECKSUM ( LC.[CategoryID], LC.[CategoryName] ) <> HC.[CheckSum]
+	WHERE		LC.[CheckSum] <> HC.[CheckSum]
 				OR HC.[CategoryID] IS NULL;
+	
+	DROP INDEX [IX_Hash_Categories_Hash] ON [Hash].[Categories];
 
 	TRUNCATE TABLE [Hash].[Categories];
 
 	INSERT INTO [Hash].[Categories]
 	SELECT		  LC.[CategoryID]
-				, CHECKSUM ( LC.[CategoryID], LC.[CategoryName] )
+				, LC.[CheckSum]
 	FROM		[Landing].[Categories] AS LC;
+
+	CREATE INDEX [IX_Hash_Categories_Hash] ON [Hash].[Categories] ( [CheckSum] )
+		INCLUDE ( [CategoryID] );
+
+	DROP INDEX [IX_Landing_Categories_Hash] ON [Landing].[Categories];
 END
