bd2814695c9e2799615321c59c03ab88bd9e055f
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-03-29 11:06:51 +0700

    #299 #301

---
+++ Landing/Landing/Store Procedures/ExtractProducts.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Landing].[ExtractProducts] AS
+BEGIN
+	UPDATE [Landing].[Products]
+	SET [CheckSum] = CHECKSUM (
+		  [ProductName]
+		, [CategoryID]
+	);
+
+	SELECT		  LP.[ProductID]
+				, LP.[ProductName]
+				, LP.[CategoryID]
+	FROM		[Landing].[Products] AS LP
+	LEFT JOIN	[Hash].[Products] AS HP ON LP.[ProductID] = HP.[ProductID]
+	WHERE		LP.[CheckSum] <> HP.[CheckSum]
+				OR HP.[ProductID] IS NULL;
+	
+	TRUNCATE TABLE [Hash].[Products];
+
+	INSERT INTO [Hash].[Products]
+	SELECT		  LP.[ProductID]
+				, LP.[CheckSum]
+	FROM		[Landing].[Products] AS LP;
+END
