8c8dc5ef5c1f5666a162697ea784617c12bf2ac0
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-04-02 21:36:43 +0700

    #303 #304

--- Landing/Hash/Stored procedure/UpdateHashOrders.sql
+++ Landing/Hash/Stored procedure/UpdateHashOrders.sql
@@ -1,18 +1,21 @@
-﻿CREATE PROCEDURE [Hash].[UpdateHashOrders] AS
-BEGIN
+﻿CREATE PROCEDURE [Hash].[UpdateHashOrders]
+	@CutoffTime AS DATE
+AS BEGIN
 	TRUNCATE TABLE [Hash].[Order Details];
 	TRUNCATE TABLE [Hash].[Orders];
+
+	INSERT INTO [Hash].[Orders]
+	SELECT		  [OrderID]
+				, [CheckSum]
+	FROM		[Landing].[Orders]
+	WHERE		[ShippedDate] <= @CutoffTime;
 
 	INSERT INTO [Hash].[Order Details]
 	SELECT		  [OrderID]
 				, [ProductID]
 				, [CheckSum]
-	FROM		[Landing].[Order Details];
-
-	INSERT INTO [Hash].[Orders]
-	SELECT		  [OrderID]
-				, [CheckSum]
-	FROM		[Landing].[Orders];
+	FROM		[Landing].[Order Details]
+	WHERE		[OrderID] IN ( SELECT [OrderID] FROM [Hash].[Orders] );
 
 	ALTER TABLE [Landing].[Order Details] DROP CONSTRAINT [PK_Landing_Order_Details];
 	ALTER TABLE [Landing].[Orders] DROP CONSTRAINT [PK_Landing_Orders];
--- Landing/Landing/Store Procedures/CreateOrdersHash.sql
+++ Landing/Landing/Store Procedures/CreateOrdersHash.sql
@@ -1,5 +1,6 @@
-﻿CREATE PROCEDURE [Landing].[CreateOrdersHash] AS
-BEGIN
+﻿CREATE PROCEDURE [Landing].[CreateOrdersHash]
+	@CutoffTime AS DATE
+AS BEGIN
 	ALTER TABLE [Landing].[Orders] ADD CONSTRAINT [PK_Landing_Orders]
 		PRIMARY KEY CLUSTERED ( [OrderID] ASC );
 	ALTER TABLE [Landing].[Order Details] ADD CONSTRAINT [PK_Landing_Order_Details]
@@ -12,12 +13,14 @@ BEGIN
 		, [OrderDate]
 		, [RequiredDate]
 		, [ShippedDate]
-	);
+	)
+	WHERE [ShippedDate] <= @CutoffTime;
 
 	UPDATE [Landing].[Order Details]
 	SET [CheckSum] = CHECKSUM (
 		  [UnitPrice]
 		, [Quantity]
 		, [Discount]
-	);
+	)
+	WHERE [OrderID] IN ( SELECT [OrderID] FROM [Landing].[Orders] WHERE [ShippedDate] <= @CutoffTime );
 END
