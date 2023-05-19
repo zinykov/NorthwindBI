a4a14f22a4c013962bf81ec329ccccd054b88420
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-05-19 23:28:41 +0300

    #307

--- cmd/RunasUserBI.bat
+++ cmd/RunasUserBI.bat
@@ -1,3 +1,4 @@
-RunAs /user:UserBI "\"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe\" http://swift3/reports/browse/Monitoring"
-
+::RunAs /user:UserBI "\"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe\" http://swift3/reports/browse/Monitoring"
 ::RunAs /user:UserBI "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\Ssms.exe"
+
+RunAs /noprofile /user:AzPipelineAgent "C:\Users\zinyk\source\repos\Northwind_BI_Solution\cmd\ExecuteTestPackage1.bat"
