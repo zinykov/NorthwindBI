6ab9cdc56433971a59546a02b014a5382c0ba5ee
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-05-30 23:10:17 +0300

    #307

--- cmd/logman.bat
+++ cmd/logman.bat
@@ -1,3 +1,1 @@
-::logman import -name "SQL Server" -xml "C:\Users\zinyk\source\repos\Northwind_BI_Solution\Monitoring\PerformanceMonitoring.xml"
-logman start -name "SQL Server"
-::/C "logman stop -name "SQL Server" -as"
+/C "CALL "logman start -name "SQL Server"""
