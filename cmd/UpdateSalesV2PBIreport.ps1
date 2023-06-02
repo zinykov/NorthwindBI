4dd3e93271a817123c0ef53224598b935bcac736
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-06-02 16:58:53 +0300

    #310

--- cmd/UpdateSalesV2PBIreport.ps1
+++ cmd/UpdateSalesV2PBIreport.ps1
@@ -1,26 +1,22 @@
-Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell_ise.exe" -Credential "SWIFT3\AzPipelineAgent"
+param ( [String]$password="P@55w.rd", [String]$PBIRSServerName="SWIFT3" )
 
-$password = ConvertTo-SecureString "P@55w.rd" -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential ("PBIRSrelease", $password)
-$WebSession = New-RsRestSession -ReportPortalUri "http://swift3/reports" -RestApiVersion v2.0 -Credential $Cred
-
-Write-RsRestFolderContent `
-	-Path:"C:\Users\zinyk\source\repos\Northwind_BI_Solution\NorthwindPBIRS\Sales" `
-	-RsFolder:"/" `
-	-Overwrite:$true `
-	-WebSession:$WebSession
+$passwordSS = ConvertTo-SecureString $password -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PSCredential ("PBIRSrelease", $passwordSS)
+$ReportPortalUri = "http://" + $PBIRSServerName + "/reports"
+$WebSession = New-RsRestSession -ReportPortalUri:$ReportPortalUri -RestApiVersion:v2.0 -Credential:$Cred
 
 $RsItem = "/SalesV2"
+$Username = $PBIRSServerName + "\RDLexec"
 
 $dataSources = Get-RsRestItemDataSource -RsItem:$RsItem -WebSession:$WebSession
 $dataSources[0].DataModelDataSource.AuthType = "Windows"
-$dataSources[0].DataModelDataSource.Username = "SWIFT3\RDLexec"
-$dataSources[0].DataModelDataSource.Secret = "P@55w.rd"
+$dataSources[0].DataModelDataSource.Username = $Username
+$dataSources[0].DataModelDataSource.Secret = $password
 
 Set-RsRestItemDataSource -RsItem:$RsItem -RsItemType:PowerBIReport -DataSources:$datasources -WebSession:$WebSession
 
 New-RsRestCacheRefreshPlan -RsItem:$RsItem -StartDateTime:"2023-05-31T11:00:00+03:00" -WebSession:$WebSession
 
-$ID = Get-RsRestCacheRefreshPlan -RsReport:$RsItem -WebSession:$WebSession | Select-Object -ExpandProperty:"ID"
+$ID = Get-RsRestCacheRefreshPlan -RsReport:$RsItem -WebSession:$WebSession | Select-Object -first 1 -ExpandProperty:"ID" | out-string
 
 Start-RsRestCacheRefreshPlan -Id:$ID -WebSession:$WebSession
