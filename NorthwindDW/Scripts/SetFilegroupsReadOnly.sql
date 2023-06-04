9d045d79ac436f39a1255171eb8291cabc696a46
AzPipelineAgent <denis@zinykov.onmicrosoft.com>
2023-06-04 19:42:34 +0300

    #312

--- NorthwindDW/Scripts/SetFilegroupsReadOnly.sql
+++ NorthwindDW/Scripts/SetFilegroupsReadOnly.sql
@@ -5,58 +5,49 @@
 USE [master];
 GO
 
-IF ( CONVERT ( BIT , N'$(IsSetFilegroupReadonly)' ) = 1 )
-BEGIN
-	ALTER DATABASE [$(DatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-	PRINT N'[$(DatabaseName)] setted in SINGLE_USER mode'
-END;
+ALTER DATABASE [$(DatabaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+PRINT N'[$(DatabaseName)] setted in SINGLE_USER mode';
 GO
 
 USE [$(DatabaseName)];
 GO
 
-IF ( CONVERT ( BIT , N'$(IsSetFilegroupReadonly)' ) = 1 )
-BEGIN
-	DECLARE @FilegroupName AS NVARCHAR(100)
-	DECLARE @ReadOnly AS BIT
-	DECLARE @SQL AS NVARCHAR(1000)
+DECLARE @FilegroupName AS NVARCHAR(100)
+DECLARE @ReadOnly AS BIT
+DECLARE @SQL AS NVARCHAR(1000)
 
-	DECLARE FactTables CURSOR FOR
-		SELECT		[groupname]
-		FROM		sys.sysfilegroups
-		WHERE		[groupname] LIKE CONCAT ( N'%_', YEAR ( $(Cutofftime) ) - 2, N'_%' )
+DECLARE FactTables CURSOR FOR
+	SELECT		[groupname]
+	FROM		sys.sysfilegroups
+	WHERE		[groupname] LIKE CONCAT ( N'%_', YEAR ( $(Cutofftime) ) - 2, N'_%' )
 
-	OPEN FactTables
-		FETCH NEXT FROM FactTables INTO @FilegroupName
-		WHILE @@FETCH_STATUS = 0
-			BEGIN
-				SELECT @readonly = [is_read_only] FROM sys.filegroups WHERE [name] = @FilegroupName
-				IF ( @readonly = 0 )
-					BEGIN
-						SET @SQL = CONCAT (
-							  N'ALTER DATABASE [$(DatabaseName)] MODIFY FILEGROUP ['
-							, @FilegroupName
-							, N'] READONLY'
-						)
-						EXECUTE sp_executesql @SQL
+OPEN FactTables
+	FETCH NEXT FROM FactTables INTO @FilegroupName
+	WHILE @@FETCH_STATUS = 0
+		BEGIN
+			SELECT @readonly = [is_read_only] FROM sys.filegroups WHERE [name] = @FilegroupName
+			IF ( @readonly = 0 )
+				BEGIN
+					SET @SQL = CONCAT (
+							N'ALTER DATABASE [$(DatabaseName)] MODIFY FILEGROUP ['
+						, @FilegroupName
+						, N'] READONLY'
+					)
+					EXECUTE sp_executesql @SQL
 
-						UPDATE	[Maintenance].[DatabaseFiles]
-						SET		[IsReadOnly] = 1
-						WHERE	[GroupName] = @FilegroupName
-					END
-				FETCH NEXT FROM FactTables INTO @FilegroupName
-			END
-	CLOSE FactTables
-	DEALLOCATE FactTables
-END;
+					UPDATE	[Maintenance].[DatabaseFiles]
+					SET		[IsReadOnly] = 1
+					WHERE	[GroupName] = @FilegroupName
+				END
+			FETCH NEXT FROM FactTables INTO @FilegroupName
+		END
+CLOSE FactTables
+DEALLOCATE FactTables;
 GO
 
 USE [master];
 GO
 
-IF ( CONVERT ( BIT , N'$(IsSetFilegroupReadonly)' ) = 1 )
-BEGIN
-	ALTER DATABASE [$(DatabaseName)] SET MULTI_USER
-	PRINT N'[$(DatabaseName)] setted in MULTI_USER mode'
-END;
+ALTER DATABASE [$(DatabaseName)] SET MULTI_USER
+PRINT N'[$(DatabaseName)] setted in MULTI_USER mode';
 GO
