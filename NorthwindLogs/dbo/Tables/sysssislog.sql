c953dfff293ab83ce3ee604d7c4ced942f37fe3c
Зинуков Денис <denis@zinykov.onmicrosoft.com>
2023-10-29 22:49:07 +0300

    #513

---
+++ NorthwindLogs/dbo/Tables/sysssislog.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[sysssislog] (
+	  [Id]			INT					IDENTITY(1,1)	NOT NULL
+	, [event]		SYSNAME				NOT NULL
+	, [computer]	NVARCHAR(128)		NOT NULL
+	, [operator]	NVARCHAR(128)		NOT NULL
+	, [source]		NVARCHAR(1024)		NOT NULL
+	, [sourceid]	UNIQUEIDENTIFIER	NOT NULL
+	, [executionid]	UNIQUEIDENTIFIER	NOT NULL
+	, [starttime]	datetime			NOT NULL
+	, [endtime]		datetime			NOT NULL
+	, [datacode]	INT					NOT NULL
+	, [databytes]	IMAGE				NULL
+	, [message]		NVARCHAR(2048)		NOT NULL
+)
