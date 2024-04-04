7d0a05f8a41683ba323f18bf436f3fc05d6071d4
Denis Zinukov <ZinukovD@terralink-global.com>
2024-04-04 08:30:50 +0700

    #594

--- NorthwindDW/Scripts/ErrorMessages.sql
+++ NorthwindDW/Scripts/ErrorMessages.sql
@@ -4,7 +4,7 @@ GO
 EXECUTE sp_addmessage
 	  @MSGNUM = 50001
 	, @severity = 16
-	, @msgtext = N'Could not find directory ''%s'''
+	, @msgtext = N'Error #%i in line %i Could not find directory ''%s'''
 	, @lang = N'us_english'
 	, @with_log = TRUE
 	, @replace = 'replace';
@@ -13,7 +13,26 @@ GO
 EXECUTE sp_addmessage
 	  @MSGNUM = 50001
 	, @severity = 16
-	, @msgtext = N'Не удаётся найти папку ''%s'''
+	, @msgtext = N'Ошибка №%i в строке %i Не удаётся найти папку ''%s'''
+	, @lang = N'russian'
+	, @replace = 'replace';
+GO
+
+
+
+EXECUTE sp_addmessage
+	  @MSGNUM = 50002
+	, @severity = 16
+	, @msgtext = N'Error #%i in line %i with error message ''%s'''
+	, @lang = N'us_english'
+	, @with_log = TRUE
+	, @replace = 'replace';
+GO
+
+EXECUTE sp_addmessage
+	  @MSGNUM = 50002
+	, @severity = 16
+	, @msgtext = N'Ошибка №%i в строке %i, сообщение об ошибке ''%s'''
 	, @lang = N'russian'
 	, @replace = 'replace';
 GO
