1080592b22523d8a276c8066a5cdbb43a95a9439
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2024-05-26 10:34:54 +0300

    #513

--- NorthwindETLTest/NorthwindETLTest.cs
+++ NorthwindETLTest/NorthwindETLTest.cs
@@ -193,7 +193,7 @@ namespace NorthwindETLTest
                         $", [EmployeeID]" +
                         $", [OrderDate]" +
                         $", [RequiredDate]" +
-                        $", CASE WHEN [ShippedDate] > DATEFROMPARTS(1899, 12, 30) THEN NULL ELSE [ShippedDate] END AS [ShippedDate]" +
+                        $", CASE WHEN [ShippedDate] > DATEFROMPARTS({CutoffTime.Year}, {CutoffTime.Month}, {CutoffTime.Day}) THEN NULL ELSE [ShippedDate] END AS [ShippedDate]" +
                         $", [ShipCity]" +
                         $", [ShipCountry]" +
                         $" FROM [Landing].[Orders]" +
