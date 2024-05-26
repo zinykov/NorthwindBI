c1e5ebfac993a0dce8e4d6b0c3142522a0acf86d
Денис Зинуков <denis@zinykov.onmicrosoft.com>
2024-05-26 10:33:12 +0300

    #513

--- Scripts/Runas.bat
+++ Scripts/Runas.bat
@@ -1,2 +1,2 @@
 ::RunAs /user:UserBI "\"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe\" http://swift3/reports/browse/Monitoring"
-RunAs /user:RDLexec "C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\Ssms.exe"
+RunAs /user:azPipelineAgent "C:\Program Files (x86)\Microsoft SQL Server Management Studio 20\Common7\IDE\Ssms.exe"
