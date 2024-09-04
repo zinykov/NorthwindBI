ab984e632d591c1fa1a6ad05aeeaa564eb1c6575
Денис Зинуков <zinykov@hotmail.com>
2024-09-04 21:58:09 +0300

    #657

--- NorthwindDW/Integration/Store Procedures/AddUnknownProduct.sql
+++ NorthwindDW/Integration/Store Procedures/AddUnknownProduct.sql
@@ -2,27 +2,20 @@
       @StartDate AS DATE
     , @LineageKey AS INT
 AS BEGIN
-    --BEGIN TRY
-    --    BEGIN TRANSACTION
-            IF NOT EXISTS ( SELECT 1 FROM [Dimension].[Product] WHERE [ProductKey] = -1 )
-            INSERT INTO [Dimension].[Product] (
-                  [ProductKey]
-                , [ProductAlterKey]
-                , [Product]
-                , [Category]
-                , [LineageKey]
-            ) VALUES (
-                  -1
-                , -1
-                , 'N/A'
-                , 'N/A'
-                , @LineageKey
-            )
-  --      COMMIT TRANSACTION;
-  --  END TRY
-  --  BEGIN CATCH
-  --      ROLLBACK TRANSACTION;
-  --      DECLARE @Msg AS NVARCHAR(2048) = FORMATMESSAGE(50002, ERROR_NUMBER(), ERROR_LINE(), ERROR_MESSAGE());
-		--THROW 50002, @Msg, 1;
-  --  END CATCH
+    IF NOT EXISTS ( SELECT 1 FROM [Dimension].[Product] WHERE [ProductKey] = -1 )
+    INSERT INTO [Dimension].[Product] (
+          [ProductKey]
+        , [ProductAlterKey]
+        , [Product]
+        , [Category]
+        , [AllAttributes]
+        , [LineageKey]
+    ) VALUES (
+            -1
+        , -1
+        , N'N/A'
+        , N'N/A'
+        , N'{}'
+        , @LineageKey
+    )
 END
