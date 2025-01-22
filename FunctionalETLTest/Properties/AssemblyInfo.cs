7cc76eea367dc28fadea29e3967a4247f34e4567
Denis Zinukov <ZinukovD@terralink-global.com>
2025-01-22 22:28:46 +0300

    #694

---
+++ FunctionalETLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FunctionalETLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Terralink")]
+[assembly: AssemblyProduct("FunctionalETLTest")]
+[assembly: AssemblyCopyright("Copyright © Terralink 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("578a36e7-b5ae-4a1b-9170-56d591e102e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NorthwindDWTest/OverwritedTestService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Schema.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace NorthwindDWTest
+{
+    public class OverwritedTestService : SqlDatabaseTestService
+    {
+        private SqlConnectionStringBuilder sqlConnectionString;
+
+        public OverwritedTestService(TestContext TestContext)
+        {
+            sqlConnectionString = new SqlConnectionStringBuilder();
+            sqlConnectionString.ApplicationName = "FunctionalETLTest";
+            sqlConnectionString.DataSource = Environment.MachineName;
+            sqlConnectionString.InitialCatalog = (string)TestContext.Properties["DWHDatabaseName"];
+            sqlConnectionString.IntegratedSecurity = true;
+            sqlConnectionString.PersistSecurityInfo = false;
+            sqlConnectionString.Pooling = false;
+            sqlConnectionString.MultipleActiveResultSets = false;
+            sqlConnectionString.ConnectTimeout = 60;
+            sqlConnectionString.Encrypt = true;
+            sqlConnectionString.TrustServerCertificate = true;
+        }
+
+        public override ConnectionContext OpenExecutionContext()
+        {
+            ConnectionContext connectionContext = new ConnectionContext();
+            connectionContext.Provider = DbProviderFactories.GetFactory("System.Data.SqlClient");
+            connectionContext.Provider.CreateConnection();
+            connectionContext.Connection = new SqlConnection(sqlConnectionString.ToString());
+            connectionContext.Connection.Open();
+            return connectionContext;
+        }
+
+        public override ConnectionContext OpenPrivilegedContext()
+        {
+            ConnectionContext connectionContext = new ConnectionContext();
+            connectionContext.Provider = DbProviderFactories.GetFactory("System.Data.SqlClient");
+            connectionContext.Provider.CreateConnection();
+            connectionContext.Connection = new SqlConnection(sqlConnectionString.ToString());
+            connectionContext.Connection.Open();
+            return connectionContext;
+        }
+    }
+}
